e182e2bec3cfc9a1529672c0aa216ca5ad864708
scostello-hubQA <scostello@academytrainee.com>
2021-02-09 15:56:45 +0000

    create, read all, delete for to do js

--- src/main/resources/static/ToDo.js
+++ src/main/resources/static/ToDo.js
@@ -1,40 +1,48 @@
 'use strict';
 
 const toDoCreate = document.querySelector('#toDoTitle');
-const deleteToDo1 = document.querySelector('#toDoDeleteId')
+const deleteToDo1 = document.querySelector('#toDoDeleteId');
+const peeps = document.querySelector('#peeps');
+
+
+
+
+
+
 
 
 const createToDo = () => {
 	const toDoValue = toDoCreate.value;
-	
+
 	let data = {
-		
+
 		"name": toDoValue
 	}
-	
+
 	fetch("http://localhost:8080/todo/create", {
-		method : "POST",
+		method: "POST",
 		body: JSON.stringify(data),
 		headers: {
-			"Content-Type" :"application/json"
+			"Content-Type": "application/json"
 		}
 	}).then(response => response.json())
-	.then(info => console.log(info))
-	.catch(err => console.error('something went wrong! ${err}'));
-	
+		.then(info => console.log(info))
+		.catch(err => console.error('something went wrong! ${err}'));
+
 }
 
-const toDoReadAll =() => {
+const toDoReadAll = () => {
 	fetch("http://localhost:8080/todo/readAll")
-			.then((response) => {
+		.then((response) => {
 			if (response.status != 200) {
 				throw new Error(`i dont have a status of 200`);
 			} else {
 				console.log(response);
 				console.log(`response is okay (200)`);
 				response.json().then((infofromserver) => {
-					console.log(infofromserver);
-					//     console.log(infofromserver.data);
+
+					console.log(infofromserver)
+
 				})
 			}
 		}).catch((err) => {
@@ -42,16 +50,16 @@ const toDoReadAll =() => {
 		})
 }
 
-const deleteToDo =() => {
+const deleteToDo = () => {
 	const toDoDelete = deleteToDo1.value
-	
+
 	let data = {
-		"id":toDoDelete
-		
+		"id": toDoDelete
+
 	}
-	fetch(`http://localhost:8080/todo/delete/${toDoDelete}`,{
-		
-			method: "DELETE",
+	fetch(`http://localhost:8080/todo/delete/${toDoDelete}`, {
+
+		method: "DELETE",
 	}).then(response => console.log(`to do with id ${toDoDelete} deleted`))
 		.catch(err => console.error(`Stop!! ${err}`));
 }
